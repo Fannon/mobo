f83b8af3659fba0fa0cfa5c1b3e12f32db345094
Fannon <heimlersimon@gmail.com>
2014-09-27 09:10:16 +0200

    * Fixed package.json for NPM (devDeps) * Better warning messages / behaviour

--- cli.js
+++ cli.js
@@ -54,8 +54,11 @@ if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
 }
 
 if (userArgs.indexOf('-c') !== -1 || userArgs.indexOf('--config') !== -1) {
-    console.log('Currently used settings:');
-    return console.log(JSON.stringify(settings, false, 4));
+    if (settings) {
+        log('> Currently used settings:');
+        log(settings);
+    }
+    return;
 }
 
 if (userArgs.indexOf('-i') !== -1 || userArgs.indexOf('--init') !== -1 ) {
@@ -89,106 +92,130 @@ if (settings) {
     // SERVE WEBAPP                         //
     //////////////////////////////////////////
 
-    var file = new fileServer.Server(__dirname + '/webapp');
+    if (settings.serveWebapp) {
+        var file = new fileServer.Server(__dirname + '/webapp');
 
-    log('');
-    log('> [INFO] Serving the webapp at http://localhost:' + settings.port + '/');
+        log('');
+        log('> [INFO] Serving the webapp at http://localhost:' + settings.port + '/');
 
-    require('http').createServer(function (request, response) {
+        var webserver = require('http').createServer(function (request, response) {
 
-        request.addListener('end', function () {
+            request.addListener('end', function () {
 
-            if (request.url === '/_processed/_registry.js' ||
-                request.url === '/_processed/_generated.js'||
-                request.url === '/_processed/_graph_layouted.gexf' ) {
+                if (request.url === '/_processed/_registry.js' ||
+                    request.url === '/_processed/_generated.js'||
+                    request.url === '/_processed/_graph_layouted.gexf' ) {
 
-                var filename = path.join(process.cwd(), request.url);
-                fs.exists(filename, function(exists) {
-                    if(!exists) {
-                        console.log("> 404 Not Found: " + filename);
-                        response.writeHead(200, {'Content-Type': 'text/plain'});
-                        response.write('404 Not Found\n');
-                        response.end();
-                    } else {
-                        response.writeHead(200, 'text/javascript');
-                        var fileStream = fs.createReadStream(filename);
-                        fileStream.pipe(response);
-                    }
-                });
+                    var filename = path.join(process.cwd(), request.url);
+                    fs.exists(filename, function(exists) {
+                        if(!exists) {
+                            console.log("> 404 Not Found: " + filename);
+                            response.writeHead(200, {'Content-Type': 'text/plain'});
+                            response.write('404 Not Found\n');
+                            response.end();
+                        } else {
+                            response.writeHead(200, 'text/javascript');
+                            var fileStream = fs.createReadStream(filename);
+                            fileStream.pipe(response);
+                        }
+                    });
 
-            } else  {
-                file.serve(request, response);
+                } else  {
+                    file.serve(request, response);
+                }
+
+            }).resume();
+
+        });
+
+        webserver.listen(settings.port);
+
+        webserver.on('error', function(err) {
+            if (err.errno === 'EADDRINUSE') {
+                log('> [ERROR] Webserver failed, port ' + settings.port + ' is already in use!');
+            } else {
+                console.log(err);
             }
 
-        }).resume();
+        });
+    }
 
-    }).listen(settings.port);
+
 
 
     //////////////////////////////////////////
     // WATCH MODEL (FILESYSTEM)             //
     //////////////////////////////////////////
 
-    var chokidar = require('chokidar');
+    if (settings.watchFilesystem) {
+        var chokidar = require('chokidar');
 
-    log('> [INFO] Watching for changes in the filesystem');
-    log('> [INFO] Enter q to quit the interactive mode');
+        log('> [INFO] Watching for changes in the filesystem');
 
-    // Create filesystem watcher
-    var watcher = chokidar.watch(
-        [
-            cwd + '/settings.json',
-            cwd + '/field',
-            cwd + '/form',
-            cwd + '/model',
-            cwd + '/smw_query',
-            cwd + '/smw_site',
-            cwd + '/smw_template',
-            cwd + '/templates'
-        ],
-        {
-            ignored: /[\/\\]\./,
-            persistent: true
-        }
-    );
+        // Create filesystem watcher
+        var watcher = chokidar.watch(
+            [
+                    cwd + '/settings.json',
+                    cwd + '/field',
+                    cwd + '/form',
+                    cwd + '/model',
+                    cwd + '/smw_query',
+                    cwd + '/smw_site',
+                    cwd + '/smw_template',
+                    cwd + '/templates'
+            ],
+            {
+                ignored: /[\/\\]\./,
+                persistent: true
+            }
+        );
 
-    // Watcher events
-    watcher
-        .on('change', function(file) {
+        // Watcher events
+        watcher
+            .on('change', function(file) {
 
-            log('');
-            log(' C File changed: ' + path.basename(file) + '');
+                log('');
+                log(' C File changed: ' + path.basename(file) + '');
 
-            // Update Settings, generate new model
-            settings = mobo.getSettings();
-            mobo.run(settings);
+                // Update Settings, generate new model
+                settings = mobo.getSettings();
+                mobo.run(settings);
 
-        })
-        .on('error', function(error) {
-            log('> [ERROR] Watching failed with error', error);
-        })
-    ;
+            })
+            .on('error', function(error) {
+                log('> [ERROR] Watching failed with error', error);
+            })
+        ;
+    }
+
+
 
     //////////////////////////////////////////
     // LISTEN FOR USER INPUT                //
     //////////////////////////////////////////
 
-    process.stdin.setEncoding('utf8');
+    if (settings.watchFilesystem || settings.serveWebapp) {
 
-    process.stdin.on('readable', function() {
-        var line = process.stdin.read();
+        log('> [INFO] Enter q to quit the interactive mode');
 
-        if (line !== null) {
+        process.stdin.setEncoding('utf8');
 
-            var input = line.trim();
+        process.stdin.on('readable', function() {
+            var line = process.stdin.read();
 
-            if (input === 'q' || input === 'exit' || input === 'quit') {
-                process.exit();
-            } else {
-                mobo.run(settings);
+            if (line !== null) {
+
+                var input = line.trim();
+
+                if (input === 'q' || input === 'exit' || input === 'quit') {
+                    process.exit();
+                } else {
+                    mobo.run(settings);
+                }
             }
-        }
-    });
+        });
+    }
+
 
 }
 
