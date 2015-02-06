94dc117c54c4dea015cfd24978a43f48af96fcce
Fannon <heimlersimon@gmail.com>
2015-02-06 18:09:44 +0100

    * TESTING: More unit-tests * TESTING: new mockup directory  with json objects

--- test/buildRegistry.spec.js
+++ test/buildRegistry.spec.js
@@ -6,6 +6,8 @@
 //////////////////////////////////////////
 
 var expect = require('chai').expect;
+
+var mockModel = require('./mockups/mockModel.json');
 var buildRegistry = require('../lib/model/buildRegistry.js');
 
 
@@ -13,79 +15,6 @@ var buildRegistry = require('../lib/model/buildRegistry.js');
 // MOCK OBJECTS / DATA                  //
 //////////////////////////////////////////
 
-var unorderedModel = {
-    "properties": {
-        "3": {  },
-        "1": {  },
-        "2": {  },
-        "4": {  },
-        "5": {  }
-    },
-    "propertyOrder": [
-        "1",
-        "2",
-        "3",
-        "4"
-    ]
-};
-
-var mockModel = {
-    field: {},
-    model: {},
-    form: {}
-};
-
-mockModel.field.x = {
-    "title": "x",
-    "type": "number"
-};
-mockModel.field.y = {
-    "title": "y",
-    "type": "number"
-};
-mockModel.field.color = {
-    "title": "color",
-    "type": "string",
-    "enum": [
-        "red",
-        "green",
-        "blue"
-    ]
-};
-mockModel.field.radius = {
-    "title": "radius",
-    "type": "number",
-    "minimum": 0
-};
-
-
-mockModel.model._Shape = {
-    "title": "Shape",
-    "description": "Generic Shape",
-    "type": "object",
-    "properties": {
-        "x": { "$extend": "/field/x.json" },
-        "y": { "$extend": "/field/y.json" },
-        "color": { "$extend": "/field/color.json" }
-    },
-    "required": ["x", "y"],
-    "abstract": true
-};
-
-mockModel.model.Circle = {
-    "$extend": "/model/_Shape.json",
-    "title": "Circle",
-    "type": "object",
-    "properties": {
-        "radius": {
-            "$extend": "/field/radius.json"
-        }
-    },
-    "required": ["x", "y", "radius"],
-    "smw_subobject": true,
-    "smw_category": true,
-    "abstract": false
-};
 
 describe('Registry Builder ', function() {
 
@@ -94,9 +23,28 @@ describe('Registry Builder ', function() {
      * Properties not explicitly stated in the propertyOrder array will be appended at bottom.
      */
     it('orders model properties', function() {
+
         var property;
-        var orderedModel = buildRegistry.orderObjectProperties(unorderedModel);
         var orderedModelArray = [];
+
+        /** Unordered model mock-object */
+        var unorderedModel = {
+            "properties": {
+                "3": {  },
+                "1": {  },
+                "2": {  },
+                "4": {  },
+                "5": {  }
+            },
+            "propertyOrder": [
+                "1",
+                "2",
+                "3",
+                "4"
+            ]
+        };
+
+        var orderedModel = buildRegistry.orderObjectProperties(unorderedModel);
 
         for (property in orderedModel.properties) {
             orderedModelArray.push(property);
@@ -104,12 +52,22 @@ describe('Registry Builder ', function() {
         expect(orderedModelArray).to.deep.equal(["1", "2", "3", "4", "5"]);
     });
 
+
     it('applies one-step inhertitance to models', function() {
         var extendedCircle = buildRegistry.inherit(mockModel.model.Circle, mockModel);
         expect(extendedCircle.properties.radius.title).to.equal('radius');
         expect(extendedCircle.properties.radius.$reference).to.equal('/field/radius.json');
     });
 
+    it('expands the models', function() {
+        var expandedRegistry = buildRegistry.expandModels(mockModel);
 
+        expect(expandedRegistry).to.include.keys(['_Shape', 'Circle']);
+        expect(expandedRegistry.Circle).to.include.keys('$schema');
+
+        // Check that the model properties are correctly inherited
+        expect(expandedRegistry.Circle.properties.radius.title).to.equal('radius');
+        expect(expandedRegistry.Circle.properties.radius.$reference).to.equal('/field/radius.json');
+    });
 
 });
